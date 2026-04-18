a6488dda3cedc38276186cc0c7e7f952648de61b
fuadfaruk <fuadfaruk707@gmail.com>
2026-04-18 14:34:05 +0600

    Added "Rewriter" for "history" Route

--- MyWebApp/Program.cs
+++ MyWebApp/Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.AspNetCore.Rewrite;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Welcome to Contoso!");
+app.UseRewriter(new RewriteOptions().AddRedirect("history", "about"));
 app.MapGet("/about", () => "Contoso was founded in 2000.");
 
 app.Run();
